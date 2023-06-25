f2ebc3643808473627e3d666a5cd941bd9e0b732
HieuNghiadev <nvohieunghia@gmail.com>
2023-06-25 17:55:43 +0700

    cap nhat trang login, them trang OTP, Signup

---
+++ login_2/android/app/src/main/kotlin/com/example/urtrace/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.urtrace
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- login_2/lib/components/button_login.dart
+++ login_2/lib/components/button_login.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../config/const.dart';
 
 class MyButton extends StatefulWidget {
