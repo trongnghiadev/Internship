ab6a4da4bc5b6de39593f43059d796159ea14144
HieuNghia304 <nvohieeunghia@gmail.com>
2023-07-07 20:06:52 +0700

    Otpdone

--- login_2/lib/widgets/button_bottom.dart
+++ login_2/lib/widgets/button_bottom.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:login_2/config/const.dart';
+import 'package:login_2/widgets/custom_text.dart';
 
 class ButtonBottom extends StatelessWidget {
   final String imagePath;
@@ -61,6 +63,50 @@ class ButtonBottom extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class CustomButton extends StatefulWidget {
+  final String text;
+  final VoidCallback onTap;
+  final double? width;
+
+  const CustomButton(
+      {super.key, this.width, required this.onTap, required this.text});
+
+  @override
+  State<CustomButton> createState() => _CustomButtonState();
+}
+
+class _CustomButtonState extends State<CustomButton> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: widget.onTap,
+      child: Container(
+          width: widget.width ?? 100,
+          height: 46,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              color: AppColors.buttonColor,
+              boxShadow: const [
+                BoxShadow(
+                    color: Color.fromARGB(255, 175, 173, 173),
+                    offset: Offset(4.0, 4.0),
+                    blurRadius: 15,
+                    spreadRadius: 1.0),
+                BoxShadow(
+                    color: Color.fromARGB(255, 245, 244, 244),
+                    offset: Offset(-4.0, -4.0),
+                    blurRadius: 15,
+                    spreadRadius: 1.0),
+              ]),
+          child: Center(
+            child: CustomText(
+              text: widget.text,
+            ),
+          )),
     );
   }
 }
---
+++ login_2/lib/widgets/custom_text.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CustomText extends StatelessWidget {
+  final String text;
+  final double fontSize;
+  final FontWeight fontWeight;
+  const CustomText(
+      {super.key,
+      required this.text,
+      this.fontSize = 14,
+      this.fontWeight = FontWeight.normal});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(fontSize: fontSize, fontWeight: fontWeight),
+    );
+  }
+}
